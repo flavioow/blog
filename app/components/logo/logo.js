ce9a009a134b043fda8f73b754954eb686ab3e96
Fh-Shadow <flaviohps8@gmail.com>
2024-07-09 03:58:06 -0300

    update logo

--- app/components/logo/logo.js
+++ app/components/logo/logo.js
@@ -6,7 +6,13 @@ const Logo = () => {
 		<div className='logoContainer'>
 			<a href="./">
 				<svg className='Logo' width="400" height="600" viewBox="0 0 400 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<defs>
+						<mask id="mask">
+							<rect x="-600" y="0" width="400" height="600" fill="white" className="mask-rect" />
+						</mask>
+					</defs>
 					<path className='logoPath' d="M100 0L0 150V600H300L400 450V300H300L400 150V0H100Z" fill="currentColor" />
+					<path className='logoPathOverlay' d="M100 0L0 150V600H300L400 450V300H300L400 150V0H100Z" fill="var(--theme-primary)" mask="url(#mask)" />
 				</svg>
 			</a>
 		</div>
