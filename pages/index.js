4c12927771ec58843199fe22baf299d90e53361e
Fh-Shadow <flaviohps8@gmail.com>
2024-07-09 02:44:43 -0300

    update button & theme

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 /* Components */
 import Header from '../app/layouts/header/'
+import Button from '../app/components/button/'
 
 
 function Index() {
     return (
         <div id='root'>
             <Header />
+            <Button label='Example' type='Primary' /> <br/>
+            <Button label='Example' type='Secondary' /> <br/>
+            <Button label='Example' type='Accent' /> <br/>
         </div>
     )
 }
