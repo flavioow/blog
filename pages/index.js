3152b0b5f3080c16708aa1229d4f8124bde78af4
Fh-Shadow <flaviohps8@gmail.com>
2024-07-09 03:12:22 -0300

    update button

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,13 @@ function Index() {
     return (
         <div id='root'>
             <Header />
-            <Button label='Example' type='Primary' /> <br/>
-            <Button label='Example' type='Secondary' /> <br/>
-            <Button label='Example' type='Accent' /> <br/>
+            <Button label='Example' type='Primary' className='disable' /> <br/>
+            <Button label='Example' type='Secondary' className='disable' /> <br/>
+            <Button label='Example' type='Accent' className='disable' /> <br/>
+            <Button label='Example' type='Success' className='disable' /> <br/>
+            <Button label='Example' type='Error' className='disable' /> <br/>
+            <Button label='Example' type='Warning' className='disable' /> <br/>
+            <Button label='Example' type='Basic' className='disable' /> <br/>
         </div>
     )
 }
