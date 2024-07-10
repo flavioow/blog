bb36c7fd00b4925a63ca103f2938d06923bd0a50
Fh-Shadow <flaviohps8@gmail.com>
2024-07-10 01:52:20 -0300

    update test-connection.js

--- pages/api/test-connection.js
+++ pages/api/test-connection.js
@@ -1,13 +1,17 @@
 import clientPromise from '../../lib/mongodb'
 
 export default async (req, res) => {
+    console.log('Received request for test-connection')
     try {
         const client = await clientPromise
+        console.log('Connected to MongoDB')
         const db = client.db('Cluster0')
         const collections = await db.listCollections().toArray()
+        console.log('Collections fetched')
         const collectionNames = collections.map(collection => collection.name)
         res.status(200).json({ collections: collectionNames })
     } catch (e) {
+        console.error('Error connecting to database:', e)
         res.status(500).json({ error: 'Failed to connect to database' })
     }
 }
