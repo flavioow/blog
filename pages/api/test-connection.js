5eaaa6c2b002e8b531a76afd747fb953b16177e8
Fh-Shadow <flaviohps8@gmail.com>
2024-07-10 01:05:49 -0300

    update mongodb

--- pages/api/test-connection.js
+++ pages/api/test-connection.js
@@ -3,7 +3,7 @@ import clientPromise from '../../lib/mongodb'
 export default async (req, res) => {
 	try {
 		const client = await clientPromise
-		const db = client.db('Cluster0')
+		const db = client.db()
 		const collections = await db.listCollections().toArray()
 		res.status(200).json({ collections })
 	} catch (e) {
