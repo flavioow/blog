6ad253ad6a499189f60dd7c6b4537aeb57643faa
Fh-Shadow <flaviohps8@gmail.com>
2024-07-10 01:12:53 -0300

    update mongodb

--- pages/api/test-connection.js
+++ pages/api/test-connection.js
@@ -1,12 +1,13 @@
 import clientPromise from '../../lib/mongodb'
 
 export default async (req, res) => {
-	try {
-		const client = await clientPromise
-		const db = client.db()
-		const collections = await db.listCollections().toArray()
-		res.status(200).json({ collections })
-	} catch (e) {
-		res.status(500).json({ error: 'Failed to connect to database' })
-	}
+    try {
+        const client = await clientPromise
+        const db = client.db('Cluster0')
+        const collections = await db.listCollections().toArray()
+        const collectionNames = collections.map(collection => collection.name)
+        res.status(200).json({ collections: collectionNames })
+    } catch (e) {
+        res.status(500).json({ error: 'Failed to connect to database' })
+    }
 }
