c2ad97cc87beab4c9124d894593e225b414e96f3
Fh-Shadow <flaviohps8@gmail.com>
2024-07-09 23:40:49 -0300

    create database

---
+++ pages/api/test-connection.js
@@ -0,0 +1,12 @@
+import clientPromise from '../../lib/mongodb'
+
+export default async (req, res) => {
+	try {
+		const client = await clientPromise
+		const db = client.db('Cluster0')
+		const collections = await db.listCollections().toArray()
+		res.status(200).json({ collections })
+	} catch (e) {
+		res.status(500).json({ error: 'Failed to connect to database' })
+	}
+}
