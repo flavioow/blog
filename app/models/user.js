bf7c9943041dc5e41d0fa7a0eb8582c1575c0030
Fh-Shadow <flaviohps8@gmail.com>
2024-07-10 03:04:34 -0300

    create lib & models

---
+++ app/models/user.js
@@ -0,0 +1,22 @@
+import clientPromise from '../lib/mongo'
+import { ObjectId } from 'mongodb'
+
+const User = {
+    async findById(id) {
+        const client = await clientPromise
+        return await client.db('your-db-name').collection('users').findOne({ _id: ObjectId(id) })
+    },
+
+    async findByEmail(email) {
+        const client = await clientPromise
+        return await client.db('your-db-name').collection('users').findOne({ email })
+    },
+
+    async createUser(data) {
+        const client = await clientPromise
+        const result = await client.db('your-db-name').collection('users').insertOne(data)
+        return result.ops[0]
+    },
+}
+
+export default User
